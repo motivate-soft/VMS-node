885982e89b3cd9bf722b959bafca502ddc582b10
robert-upwork <robyop.eyeco@gmail.com>
2022-01-14 22:40:34 +0700

    Fix issue due to data double sent

--- code/lib/db/dao/emaildao.js
+++ code/lib/db/dao/emaildao.js
@@ -45,7 +45,7 @@ function searchLog(criteria, callback) {
 
 function saveLog(data, callback) {
 
-	searchLog('elgEmail=' + dbu.qStr(data.email) + ' and elgEmailDate=' + dbu.qDate(data.emaildate) + ' and elgUID=' + dbu.qStr(data.uid), function(err, rows) {
+	searchLog('elgEmail=' + dbu.qStr(data.email) + ' and elgEmailData=' + dbu.qJson(data.emaildata), function(err, rows) { // + ' and elgUID=' + dbu.qStr(data.uid)
 	
         //console.log(rows);
         //callback(err, 1);
